456773d5b1396f2faa6575e49d82a53023996b72
Hero's Dad <cody.monta@gmail.com>
2019-09-14 06:51:46 -0500

    learned props

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,32 +1,36 @@
 <template>
-<div>
-  <app-header></app-header>
-  <ninjas></ninjas>
-  <app-footer></app-footer>
-</div>
+  <div>
+    <app-header></app-header>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Ninjas from './components/Ninjas';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Ninjas from "./components/Ninjas";
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'ninjas': Ninjas,
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninjas": Ninjas
   },
-  data(){
+  data() {
     return {
-      
-    }
-  },
-}
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML Wizardry", show: false },
+        { name: "Hitoshi", speciality: "Click Events", show: false },
+        { name: "Tango", speciality: "Conditionals", show: false },
+        { name: "Kami", speciality: "Webpack", show: false },
+        { name: "Yoshi", speciality: "Data Diggin", show: false }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss">
-
-
 </style>
--- vuejs-playlist/src/components/Ninjas.vue
+++ vuejs-playlist/src/components/Ninjas.vue
@@ -1,57 +1,48 @@
 <template>
-    <div id="ninjas">
-        <ul>
-            <li v-for="ninja in ninjas" v-bind:key="ninja" v-on:click="ninja.show = !ninja.show">
-                <h2>{{ ninja.name }}</h2>
-                <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
-            </li>
-        </ul>
-    </div>
-
+  <div id="ninjas">
+    <ul>
+      <li v-for="ninja in ninjas" v-bind:key="ninja" v-on:click="ninja.show = !ninja.show">
+        <h2>{{ ninja.name }}</h2>
+        <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-
 export default {
-  data(){
-    return {
-     ninjas: [
-            {name: 'Ryu', speciality: 'Vue Components', show: false},
-            {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-            {name: 'Hitoshi', speciality: 'Click Events', show: false},
-            {name: 'Tango', speciality: 'Conditionals', show: false},
-            {name: 'Kami', speciality: 'Webpack', show: false},
-            {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-            ]
-    }
+  props: {
+      ninjas: {
+          type: Array,
+      }
   },
-}
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-#ninjas{
-    width: 100%;
-    max-width: 1200px;
-    margin: 40px auto;
-    padding: 0 20px;
-    box-sizing: border-box;
+#ninjas {
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
-ul{
-    display: flex;
-    flex-wrap: wrap;
-    list-style-type: none;
-    padding: 0;
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
 }
-li{
-    flex-grow: 1;
-    flex-basis: 300px;
-    text-align: center;
-    padding: 30px;
-    border: 1px solid #222;
-    margin: 10px;
-    cursor: pointer;
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+  cursor: pointer;
 }
-
-
-
 </style>
