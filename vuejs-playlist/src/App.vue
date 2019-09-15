2cc8c2ad466c6bd46d1a3e1245c738551355e57c
Hero's Dad <cody.monta@gmail.com>
2019-09-14 22:38:12 -0500

    push

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <add-blog></add-blog>
+    <!-- <add-blog></add-blog> -->
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 import AddBlog from './components/addBlog'
+import ShowBlogs from './components/showBlogs'
 
 
 export default {
   components: {
     'add-blog': AddBlog,
+    'show-blogs': ShowBlogs
   },
 
   data() {
---
+++ vuejs-playlist/src/components/showBlogs.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" v-bind:key="blog.id" class="single-blog">
+        <h2>{{ blog.title}}</h2>
+        <article>{{ blog.body }}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        blogs: []
+    };
+  },
+  methods: {},
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+          this.blogs = data.body.slice(0, 10);
+      });
+  }
+};
+</script>
+
+<style lang="scss">
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
